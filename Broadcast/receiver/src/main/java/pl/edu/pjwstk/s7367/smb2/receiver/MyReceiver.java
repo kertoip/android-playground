97f9d795df10551cbb177b2a2bc0b66e2f4be5e5
kertoip92 <kertoip92@gmail.com>
2016-11-23 15:44:14 +0100

    Notification intent, custom permission, BroadcastReceiver permission, refactoring

--- Broadcast/receiver/src/main/java/pl/edu/pjwstk/s7367/smb2/receiver/MyReceiver.java
+++ Broadcast/receiver/src/main/java/pl/edu/pjwstk/s7367/smb2/receiver/MyReceiver.java
@@ -1,26 +1,34 @@
 package pl.edu.pjwstk.s7367.smb2.receiver;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
-/**
- * Created by pkolodziejczak on 21.11.16.
- */
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class MyReceiver extends BroadcastReceiver {
+
+    public static final String MESSAGE_KEY = "message";
+    public static final String SEND_ACTIVITY_PACKAGE = "pl.edu.pjwstk.s7367.smb2.sender";
+
+    public static int NOTIFICATION_ID = 1;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = context.getPackageManager().getLaunchIntentForPackage(SEND_ACTIVITY_PACKAGE);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(android.R.drawable.sym_def_app_icon)
-                        .setContentTitle("My notification")
-                        .setContentText(intent.getStringExtra("message"));
+                        .setContentTitle(context.getString(R.string.notification_title))
+                        .setContentText(intent.getStringExtra(MESSAGE_KEY))
+                        .setContentIntent(notificationPendingIntent);
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, notificationBuilder.build());
-
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify((NOTIFICATION_ID+=1), notificationBuilder.build());
     }
 }
--- Broadcast/sender/src/main/java/pl/edu/pjwstk/s7367/smb2/sender/SendActivity.java
+++ Broadcast/sender/src/main/java/pl/edu/pjwstk/s7367/smb2/sender/SendActivity.java
@@ -3,25 +3,24 @@ package pl.edu.pjwstk.s7367.smb2.sender;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static java.lang.String.format;
 
 public class SendActivity extends AppCompatActivity {
 
     public static final String ACTION_NAME = "pl.edu.pjwstk.s7367.smb2.CUSTOM_ACTION";
+    public static final String MESSAGE_KEY = "message";
     EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         message = (EditText) findViewById(R.id.inputMessage);
 
@@ -30,37 +29,12 @@ public class SendActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String msg = message.getText().toString();
-
                 Intent intent= new Intent(ACTION_NAME);
-                intent.putExtra("message",msg);
+                intent.putExtra(MESSAGE_KEY, message.getText().toString());
                 sendBroadcast(intent);
 
-                Snackbar.make(view, message.getText(), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(), format("Intent with text %s has been broadcasted", message.getText()), LENGTH_SHORT ).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_send, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
